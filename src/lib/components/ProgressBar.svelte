be3e7343f331ae0c5089b5ca30d29505042a4664
coopTRUE <coopdog4545@gmail.com>
2023-09-07 16:22:12 -0700

    added progress bar

---
+++ src/lib/components/ProgressBar.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { navigating } from '$app/stores'
+  import NProgress from 'nprogress'
+  import 'nprogress/nprogress.css'
+
+  NProgress.configure({
+    showSpinner: false,
+  })
+
+  $: $navigating ? NProgress.start() : NProgress.done()
+</script>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,10 @@
-<script>
+<script lang="ts">
   import Header from '$lib/components/Header.svelte'
+  import ProgressBar from '$lib/components/ProgressBar.svelte'
   import '../app.css'
 </script>
 
+<ProgressBar />
 <div class="flex min-h-screen flex-col">
   <Header />
   <div class="flex-1">
