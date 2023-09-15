d825fecedecdee68784753dc10769e43be624479
CoopTRUE <coopdog4545@gmail.com>
2023-09-14 20:59:26 -0700

    got drizzle-kit working correctly

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,13 @@
+import dotenv from 'dotenv'
 import type { Config } from 'drizzle-kit'
+
+dotenv.config()
 
 export default {
   schema: './src/lib/server/schema/schema.ts',
+  driver: 'pg',
   out: './drizzle',
+  dbCredentials: {
+    connectionString: process.env.DATABASE_URL!,
+  },
 } satisfies Config
