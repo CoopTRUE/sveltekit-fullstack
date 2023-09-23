cc846200b27e6e474ef239cb0824bc3b8ba5ef62
CoopTRUE <coopdog4545@gmail.com>
2023-09-23 12:41:36 -0700

    better file structure

--- src/lib/trpc/middleware.ts
+++ src/lib/trpc/middleware.ts
@@ -1,5 +1,4 @@
 import { middleware } from './t'
-import { TRPCError } from '@trpc/server'
 
 export const logger = middleware(async ({ path, type, next }) => {
   const start = Date.now()
@@ -7,13 +6,4 @@ export const logger = middleware(async ({ path, type, next }) => {
   const ms = Date.now() - start
   console.log(`${result.ok ? 'OK' : 'ERR'} ${type} ${path} - ${ms}ms`)
   return result
-})
-
-export const auth = middleware(async ({ ctx, next }) => {
-  if (!ctx.session?.user) {
-    throw new TRPCError({
-      code: 'UNAUTHORIZED',
-    })
-  }
-  return next()
 })
--- src/lib/trpc/t.ts
+++ src/lib/trpc/t.ts
@@ -1,11 +1,21 @@
 import type { Context } from './context'
-import { initTRPC } from '@trpc/server'
+import { TRPCError, initTRPC } from '@trpc/server'
 import superjson from 'superjson'
 
 const t = initTRPC.context<Context>().create({
   transformer: superjson,
 })
 
-export const middleware = t.middleware
-export const router = t.router
+export const { middleware } = t
+export const auth = middleware(async ({ ctx, next }) => {
+  if (!ctx.session?.user) {
+    throw new TRPCError({
+      code: 'UNAUTHORIZED',
+    })
+  }
+  return next()
+})
+
+export const { router } = t
 export const publicProcedure = t.procedure
+export const protectedProcedure = t.procedure.use(auth)
