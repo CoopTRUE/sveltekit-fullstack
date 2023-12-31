999d3759d555916157a9dfa7a71c3836d5dc7146
coopTRUE <coopdog4545@gmail.com>
2023-12-31 00:10:40 -0700

    cleaner setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { enhancedImages } from '@sveltejs/enhanced-img'
 import { sveltekit } from '@sveltejs/kit/vite'
+import autoprefixer from 'autoprefixer'
+import tailwindcss from 'tailwindcss'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [enhancedImages(), sveltekit()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss(), autoprefixer()],
+    },
+  },
 })
