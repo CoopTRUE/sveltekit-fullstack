ec09eb0dc34c4bbb17615cf9d31427ce7b23a07f
coopTRUE <coopdog4545@gmail.com>
2024-01-12 09:31:15 -0800

    more work

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,15 @@
 import dotenv from 'dotenv'
-import type { Config } from 'drizzle-kit'
+import { defineConfig } from 'drizzle-kit'
 
 dotenv.config()
 
-export default {
+export default defineConfig({
   schema: './src/lib/server/db/schema.ts',
   driver: 'pg',
   out: './drizzle',
   dbCredentials: {
     connectionString: process.env.DATABASE_URL!,
   },
-} satisfies Config
+  verbose: true,
+  strict: true,
+})
