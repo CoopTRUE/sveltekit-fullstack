9e3074d3bff64d16dd70d32f0cbace6e7ac72780
coopTRUE <coopdog4545@gmail.com>
2024-01-14 07:13:14 -0800

    added shadcn-svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   import { signIn } from '@auth/sveltekit/client'
+  import { Button } from '$lib/components/ui/button'
   import { Smile } from 'lucide-svelte'
+
+  let clicked = 0
 </script>
 
 <svelte:head>
@@ -11,4 +14,5 @@
 <main>
   <h1 class="flex items-center text-green-500">Welcome<Smile /></h1>
   <button on:click={() => signIn('google')}>Sign In</button>
+  <Button on:click={() => clicked++}>Clicked {clicked} time{clicked ? 's' : ''}</Button>
 </main>
