ce4f8f4e460dbe088e3cae10f666eac3b1dd5d47
coopTRUE <coopdog4545@gmail.com>
2024-02-14 21:51:44 -0800

    removed db from context

--- src/lib/trpc/context.ts
+++ src/lib/trpc/context.ts
@@ -1,10 +1,9 @@
-import db from '$lib/server/db'
 import type { RequestEvent } from '@sveltejs/kit'
 import type { inferAsyncReturnType } from '@trpc/server'
 
 export async function createContext({ locals }: RequestEvent) {
   const { auth } = locals
-  return { auth, db }
+  return { auth }
 }
 
 export type Context = inferAsyncReturnType<typeof createContext>
--- src/lib/trpc/routers/user.ts
+++ src/lib/trpc/routers/user.ts
@@ -1,10 +1,11 @@
+import db from '$lib/server/db'
 import { publicProcedure, router } from '../t'
 import { TRPCError } from '@trpc/server'
 import { z } from 'zod'
 
 export const userRouter = router({
-  get: publicProcedure.input(z.object({ userId: z.string() })).query(async ({ ctx, input }) => {
-    const user = await ctx.db.query.users.findFirst({
+  get: publicProcedure.input(z.object({ userId: z.string() })).query(async ({ input }) => {
+    const user = await db.query.users.findFirst({
       columns: { email: false, emailVerified: false },
       where: (user, { eq }) => eq(user.id, input.userId),
     })
