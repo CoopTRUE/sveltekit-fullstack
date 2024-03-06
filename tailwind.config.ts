b4b2906825a07691546b0babd01adee8fb8be60c
CoopTRUE <coopdog4545@gmail.com>
2024-03-05 17:40:06 -0800

    redundant fontFamily

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from 'tailwindcss'
-import { fontFamily } from 'tailwindcss/defaultTheme'
 
 export default {
   darkMode: ['class'],
@@ -53,9 +52,6 @@ export default {
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)',
-      },
-      fontFamily: {
-        sans: ['Inter', ...fontFamily.sans],
       },
     },
   },
